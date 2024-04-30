663f42dff9444e7e74ccbd0e9da404413e61e0f8
Aryajeet Ghosh <aryajeetghosh1@gmail.com>
2024-04-30 17:26:54 +0530

    feat: tailwindcss installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import io from 'socket.io-client'
+import io from 'socket.io-client';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-6xl bg-blue-500 p-2">
       Hello
     </div>
   );
